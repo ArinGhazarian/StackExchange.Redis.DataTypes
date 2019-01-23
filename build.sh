a3139e17b2e2d23a1dd05a17deaaa1c5c78c2d38
Arin Ghazarian <arin.ghazarian@gmail.com>
2019-01-22 21:50:06 -0800

    Minor refactors.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker --version
+docker --version &> /dev/null
 if [ $? -eq 0 ]; then
-    docker start redis-test || docker run --name redis-test -p 6379:6379 -d redis
+    docker start redis-test 2> /dev/null || docker run --name redis-test -p 6379:6379 -d redis
 else
     echo "Docker is not installed. Please install Docker and try again."
 fi
