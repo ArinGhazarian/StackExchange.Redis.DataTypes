14ddb2cf7edf22fe81e1b4019d9a0a767789bbdf
Arin Ghazarian <arin.ghazarian@gmail.com>
2019-01-24 22:34:54 -0800

    Exit if no docker.

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 docker --version &> /dev/null
-if [ $? -eq 0 ]; then
-    docker start redis-test 2> /dev/null || docker run --name redis-test -p 6379:6379 -d redis
-else
+if [ $? -ne 0 ]; then
     echo "Docker is not installed. Please install Docker and try again."
+    exit 1
 fi
+
+docker start redis-test 2> /dev/null || docker run --name redis-test -p 6379:6379 -d redis
 
 dotnet test tests/StackExchange.Redis.DataTypes.Tests
 
