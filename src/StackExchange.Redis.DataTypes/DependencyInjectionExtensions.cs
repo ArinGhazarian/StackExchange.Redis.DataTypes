46483eea1eeae54e658303136e0ea3fca375f514
PrinceOwen9466 <princeowenolive@gmail.com>
2020-12-17 13:32:55 +0100

    Added ServiceCollection extension method Added Newtonsoft.Json Configuration

---
+++ src/StackExchange.Redis.DataTypes/DependencyInjectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackExchange.Redis.DataTypes
+{
+    #region Abstractions
+    public class RedisDataTypeOptions
+    {
+        public JsonSerializerSettings SerializerSettings { get; set; }
+    }
+    #endregion
+
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddStackExchangeRedisDataTypes(this IServiceCollection services, Action<RedisDataTypeOptions> setupAction = null)
+        {
+            var options = new RedisDataTypeOptions();
+
+            setupAction?.Invoke(options);
+            Extensions.SerializerSettings = options.SerializerSettings ?? new JsonSerializerSettings();
+
+            services.AddSingleton<IRedisTypeFactory, RedisTypeFactory>();
+            return services;
+        }
+    }
+}
+
